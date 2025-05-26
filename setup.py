237d032f63a375cdfb4356bdbf64dc174688c640
Javier Lianes <javi_vija94@hotmail.com>
2025-05-26 20:59:42 +0200

    set version 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='restful-checker',
-    version='0.1.2',
+    version='0.1.3',
     description='Check RESTful API compliance from OpenAPI definitions and generate HTML reports',
     author='Javi Lianes',
     author_email='jlianesglr@gmail.com',
