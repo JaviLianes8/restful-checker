09efd7eac4f4a32463af5746f4b04bc798279589
Javier Lianes <javi_vija94@hotmail.com>
2025-05-27 13:46:43 +0200

    set version 0.1.18

--- restful_checker/main.py
+++ restful_checker/main.py
@@ -1,12 +1,29 @@
 from restful_checker.core.analyzer import analyze_api
+from urllib.parse import urlparse
+
 import sys
+import requests
+import tempfile
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: restful-checker <path_to_openapi.json>")
-    else:
-        output = analyze_api(sys.argv[1])
-        print(f"✅ Report generated: {output}")
+        print("Usage: restful-checker <path_to_openapi.json or URL>")
+        sys.exit(1)
 
-if __name__ == "__main__":
-    main()
+    path = sys.argv[1]
+    parsed = urlparse(path)
+
+    if parsed.scheme in ("http", "https"):
+        response = requests.get(path)
+        if response.status_code != 200:
+            print(f"❌ Failed to download OpenAPI from {path}")
+            sys.exit(1)
+
+        suffix = '.yaml' if path.endswith(('.yaml', '.yml')) else '.json'
+        with tempfile.NamedTemporaryFile(delete=False, mode='w+', suffix=suffix) as tmp:
+            tmp.write(response.text)
+            tmp.flush()
+            path = tmp.name
+
+    output = analyze_api(path)
+    print(f"✅ Report generated: {output}")
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='restful-checker',
-    version='0.1.16',
+    version='0.1.17',
     description='Check RESTful API compliance from OpenAPI definitions and generate HTML reports',
     author='Javi Lianes',
     author_email='jlianesglr@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['pyyaml'],
+    install_requires=['pyyaml','requests'],
     entry_points={
         'console_scripts': [
             'restful-checker=restful_checker.main:main'
